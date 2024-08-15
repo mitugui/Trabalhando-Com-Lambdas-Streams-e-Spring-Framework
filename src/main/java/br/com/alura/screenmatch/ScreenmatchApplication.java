41dc5f20699f6e0f13e06d25c269c9691e97daf6
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-08-14 22:36:41 -0300

    feat: create DataConverter class for generic JSON conversion

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.models.SeriesData;
+import br.com.alura.screenmatch.services.ApiConsumption;
+import br.com.alura.screenmatch.services.DataConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+		var apiConsumption = new ApiConsumption();
+		var json =  apiConsumption.getData("http://www.omdbapi.com/?t=The+Office&apikey=");
+		System.out.println(json);
+
+		DataConverter converter = new DataConverter();
+		SeriesData data = converter.getData(json, SeriesData.class);
+		System.out.println(data);
 	}
 }
---
+++ src/main/java/br/com/alura/screenmatch/services/IDataConverter.java
@@ -0,0 +1,5 @@
+package br.com.alura.screenmatch.services;
+
+public interface IDataConverter {
+    <T> T getData(String json, Class<T> clazz);
+}
