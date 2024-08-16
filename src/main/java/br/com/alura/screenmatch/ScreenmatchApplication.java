491a8a67077adf3e643571eba992b1f746a2f125
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-08-15 22:23:12 -0300

    feat: create SeasonData model and iterate to list all seasons

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,16 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.models.EpisodeData;
+import br.com.alura.screenmatch.models.SeasonData;
 import br.com.alura.screenmatch.models.SeriesData;
 import br.com.alura.screenmatch.services.ApiConsumption;
 import br.com.alura.screenmatch.services.DataConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -34,5 +38,18 @@ public class ScreenmatchApplication implements CommandLineRunner {
 				"&apikey=");
 		EpisodeData episodeData = converter.getData(json, EpisodeData.class);
 		System.out.println(episodeData);
+
+		List<SeasonData> seasons = new ArrayList<>();
+		for (int i = 1; i <= seriesData.totalSeasons(); i++) {
+			json = apiConsumption.getData("http://www.omdbapi.com/?" +
+					"t=The+Office" +
+					"&Season="+ i +
+					"&apikey=");
+
+			SeasonData seasonData = converter.getData(json, SeasonData.class);
+			seasons.add(seasonData);
+		}
+
+		seasons.forEach(System.out::println);
 	}
 }
